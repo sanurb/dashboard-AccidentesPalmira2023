bbeb42f0f992022de8e5ea0d0947355acb031939
Santiago Urbano <david.urbano@uao.edu.co>
2023-08-09 10:14:40 -0500

    feat(app.py): add bar chart for top NmaxHipotesis accidents by hypothesis
    feat(app.py): add callback function to update heatmap based on selected date range
    feat(app.py): add date picker component for selecting date range for the heatmap

--- src/app.py
+++ src/app.py
@@ -29,6 +29,13 @@ bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 labels = ['0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81-90', '91-100']
 df_accidentes['RANGO_EDAD'] = pd.cut(df_accidentes['EDAD'], bins=bins, labels=labels, right=False)
 
+# Gráfico de barras para hipótesis de accidentes
+NmaxHipotesis = 10  # Número de hipótesis más frecuentes a mostrar
+top_hipotesis = df_accidentes['HIPOTESIS'].value_counts().nlargest(NmaxHipotesis).index.tolist()
+df_accidentes['HIPOTESIS_AJUSTADA'] = df_accidentes['HIPOTESIS'].where(df_accidentes['HIPOTESIS'].isin(top_hipotesis), 'Otras')
+df_hipotesis_ajustada = df_accidentes['HIPOTESIS_AJUSTADA'].value_counts().reset_index()
+df_hipotesis_ajustada.columns = ['HIPOTESIS_AJUSTADA', 'CUENTA']
+
 MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZHNhbnR1cmJhbiIsImEiOiJjbGwxaWhpNGQwMGxkM3BtaWlidDlpOTdqIn0.Q2nMClAqoAHrwIktR-kcKA"
 px.set_mapbox_access_token(MAPBOX_ACCESS_TOKEN)
 
@@ -47,7 +54,15 @@ fig_mapa = px.scatter_mapbox(df_accidentes,
                              zoom=12,
                              center={"lat": 3.5391, "lon": -76.3035},
                              mapbox_style="mapbox://styles/mapbox/streets-v11")
-
+fig_hipotesis_ajustada = px.bar(
+    df_hipotesis_ajustada,
+    x='HIPOTESIS_AJUSTADA',
+    y='CUENTA',
+    title='Top {} Accidentes por Hipótesis'.format(NmaxHipotesis),
+    labels={'HIPOTESIS_AJUSTADA': 'Hipótesis', 'CUENTA': 'Cantidad de Accidentes'},
+    color='CUENTA',
+    color_continuous_scale=px.colors.sequential.Blues
+)
 
 # Crear la aplicación Dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -75,6 +90,7 @@ year_dropdown = dcc.Dropdown(
      Output('condicion-victima-plot', 'figure')],
     [Input('year-dropdown', 'value')]
 )
+
 def update_graphs(selected_year):
     filtered_df = df_accidentes[df_accidentes['AÑO'] == selected_year]
 
@@ -90,6 +106,47 @@ def update_graphs(selected_year):
 
     return fig_dia_updated, fig_mes_updated, fig_barrio_updated, fig_zona_updated, fig_gravedad_updated, fig_rango_edad_updated, fig_condicion_victima_updated
 
+
+# Callback para el heatmap
+@app.callback(
+    Output('heatmap', 'figure'),
+    [Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date')]
+)
+
+def update_heatmap(start_date, end_date):
+    # Filtrar el dataframe según las fechas seleccionadas
+    df_filtered = df_accidentes[(df_accidentes['FECHA'] >= start_date) & (df_accidentes['FECHA'] <= end_date)]
+
+    # Agrupa por barrio y calcula la latitud y longitud promedio y cuenta los accidentes
+    df_grouped = df_filtered.groupby('BARRIOS-CORREGIMIENTO- VIA').agg(
+        LAT=pd.NamedAgg(column='LAT', aggfunc='mean'),
+        LONG=pd.NamedAgg(column='LONG', aggfunc='mean'),
+        COUNT=pd.NamedAgg(column='BARRIOS-CORREGIMIENTO- VIA', aggfunc='count')
+    ).reset_index()
+
+    # Crear mapa de calor basado en la densidad por barrio
+    fig = px.density_mapbox(df_grouped, lat='LAT', lon='LONG',
+                            z='COUNT', radius=10,
+                            center=dict(lat=3.53, lon=-76.3), zoom=12,
+                            mapbox_style="stamen-terrain",
+                            title="Mapa de Calor de Accidentes por Barrio")
+
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    return fig
+
+# Componente de selección de fecha para el heatmap
+date_picker = html.Div([
+    html.Label("Seleccione rango de fechas:"),
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        start_date=df_accidentes['FECHA'].min(),
+        end_date=df_accidentes['FECHA'].max(),
+        display_format='DD/MM/YYYY'
+    ),
+], style={'marginBottom': 20, 'marginTop': 20})
 
 # Definir el layout de la aplicación
 app.layout = dbc.Container([
@@ -118,6 +175,15 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(dcc.Graph(id='rango-edad-plot', figure=fig_rango_edad), width=6),
         dbc.Col(dcc.Graph(id='condicion-victima-plot', figure=fig_condicion_victima), width=6)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='grafica-hipotesis', figure=fig_hipotesis_ajustada), width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(date_picker, width=12)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='heatmap'), width=12)
     ]),
     dbc.Row([
         dbc.Col(dcc.Graph(figure=fig_mapa), width=12)
