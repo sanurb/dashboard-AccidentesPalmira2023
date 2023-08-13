22db8dbdde500ce6b4b6ddc7fbd7b38a086f0cc2
Santiago Urbano <david.urbano@uao.edu.co>
2023-08-13 16:04:55 -0500

    refactor(app.py): improve code readability by adding line breaks and indentation feat(app.py): add support for converting "FECHA" column to datetime using the appropriate format feat(app.py): add support for converting "AÑO" column to integer if it is of type string feat(app.py): add support for creating age ranges based on the "EDAD" column feat(app.py): add support for mapping "BARRIOS-CORREGIMIENTO- VIA" column to "COMUNA" column feat(app.py): add support for calculating the count and percentage of accidents per "COMUNA" feat(app.py): add support for adjusting the "HIPOTESIS" column by keeping only the top N most frequent values feat(app.py): add support for creating visualizations using Plotly and Mapbox
    refactor(app.py): reformat code for better readability
    feat(app.py): add callback for updating graphs
    
    refactor(app.py): reformat code for better readability
    feat(app.py): add support for updating graphs based on selected year
    
    refactor(app.py): reformat code for better readability
    fix(app.py): fix typo in comment
    feat(app.py): add date_picker component for heatmap selection

--- src/app.py
+++ src/app.py
@@ -16,47 +16,92 @@ df_accidentes = pd.read_csv(
 )
 
 # Convertimos la columna "FECHA" a datetime utilizando el formato adecuado
-df_accidentes['FECHA'] = df_accidentes['FECHA'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y %I:%M:%S %p'))
+df_accidentes['FECHA'] = df_accidentes['FECHA'].apply(
+    lambda x: datetime.strptime(x, '%m/%d/%Y %I:%M:%S %p'))
 
 # Crear columna 'MES' basada en la columna 'FECHA'
 df_accidentes['MES'] = df_accidentes['FECHA'].dt.month
 
 # Convertimos la columna "AÑO" solo si es de tipo string
 if df_accidentes['AÑO'].dtype == 'object':
-    df_accidentes['AÑO'] = df_accidentes['AÑO'].str.replace(',', '').astype(int)
+    df_accidentes['AÑO'] = df_accidentes['AÑO'].str.replace(
+        ',', '').astype(int)
 
 # Crear rangos de edad
 bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-labels = ['0-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81-90', '91-100']
-df_accidentes['RANGO_EDAD'] = pd.cut(df_accidentes['EDAD'], bins=bins, labels=labels, right=False)
+labels = [
+    '0-10',
+    '11-20',
+    '21-30',
+    '31-40',
+    '41-50',
+    '51-60',
+    '61-70',
+    '71-80',
+    '81-90',
+    '91-100']
+df_accidentes['RANGO_EDAD'] = pd.cut(
+    df_accidentes['EDAD'],
+    bins=bins,
+    labels=labels,
+    right=False)
 
 # Columna para las comunas
-df_accidentes['COMUNA'] = df_accidentes['BARRIOS-CORREGIMIENTO- VIA'].map(barrio_comuna_mapping)
+df_accidentes['COMUNA'] = df_accidentes['BARRIOS-CORREGIMIENTO- VIA'].map(
+    barrio_comuna_mapping)
 total_accidentes = len(df_accidentes)
 comunas_count = df_accidentes['COMUNA'].value_counts().reset_index()
 comunas_count.columns = ['COMUNA', 'CUENTA']
-comunas_percent = (df_accidentes['COMUNA'].value_counts() / total_accidentes * 100).reset_index()
+comunas_percent = (
+    df_accidentes['COMUNA'].value_counts() /
+    total_accidentes *
+    100).reset_index()
 comunas_percent.columns = ['COMUNA', 'PORCENTAJE']
 comunas_percent = comunas_percent.merge(comunas_count, on="COMUNA")
 
 # Gráfico de barras para hipótesis de accidentes
 NmaxHipotesis = 15  # Número de hipótesis más frecuentes a mostrar
-top_hipotesis = df_accidentes['HIPOTESIS'].value_counts().nlargest(NmaxHipotesis).index.tolist()
-df_accidentes['HIPOTESIS_AJUSTADA'] = df_accidentes['HIPOTESIS'].where(df_accidentes['HIPOTESIS'].isin(top_hipotesis), 'Otras')
-df_hipotesis_ajustada = df_accidentes['HIPOTESIS_AJUSTADA'].value_counts().reset_index()
+top_hipotesis = df_accidentes['HIPOTESIS'].value_counts().nlargest(
+    NmaxHipotesis).index.tolist()
+df_accidentes['HIPOTESIS_AJUSTADA'] = df_accidentes['HIPOTESIS'].where(
+    df_accidentes['HIPOTESIS'].isin(top_hipotesis), 'Otras')
+df_hipotesis_ajustada = df_accidentes['HIPOTESIS_AJUSTADA'].value_counts(
+).reset_index()
 df_hipotesis_ajustada.columns = ['HIPOTESIS_AJUSTADA', 'CUENTA']
 
 MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZHNhbnR1cmJhbiIsImEiOiJjbGwxaWhpNGQwMGxkM3BtaWlidDlpOTdqIn0.Q2nMClAqoAHrwIktR-kcKA"
 px.set_mapbox_access_token(MAPBOX_ACCESS_TOKEN)
 
 # Crear visualizaciones
-fig_mes = px.histogram(df_accidentes, x="MES", title="Accidentes por Mes (2022-2023)")
-fig_dia_semana = px.histogram(df_accidentes, x="DIA_SEMANA", title="Accidentes por Día de la Semana (2022-2023)")
-fig_barrio = px.histogram(df_accidentes, x="BARRIOS-CORREGIMIENTO- VIA", title="Accidentes por Barrio (2022-2023)")
-fig_zona = px.histogram(df_accidentes, x="ZONA", title="Accidentes por Zona (2022-2023)")
-fig_gravedad = px.pie(df_accidentes, names="GRAVEDAD", title="Gravedad de las lesiones")
-fig_rango_edad = px.histogram(df_accidentes, x="RANGO_EDAD", color="GENERO", title="Distribución de Accidentes por Rango de Edad y Género")
-fig_condicion_victima = px.histogram(df_accidentes, x="CONDICION DE LA VICTIMA", title="Condición de la Víctima")
+fig_mes = px.histogram(
+    df_accidentes,
+    x="MES",
+    title="Accidentes por Mes (2022-2023)")
+fig_dia_semana = px.histogram(
+    df_accidentes,
+    x="DIA_SEMANA",
+    title="Accidentes por Día de la Semana (2022-2023)")
+fig_barrio = px.histogram(
+    df_accidentes,
+    x="BARRIOS-CORREGIMIENTO- VIA",
+    title="Accidentes por Barrio (2022-2023)")
+fig_zona = px.histogram(
+    df_accidentes,
+    x="ZONA",
+    title="Accidentes por Zona (2022-2023)")
+fig_gravedad = px.pie(
+    df_accidentes,
+    names="GRAVEDAD",
+    title="Gravedad de las lesiones")
+fig_rango_edad = px.histogram(
+    df_accidentes,
+    x="RANGO_EDAD",
+    color="GENERO",
+    title="Distribución de Accidentes por Rango de Edad y Género")
+fig_condicion_victima = px.histogram(
+    df_accidentes,
+    x="CONDICION DE LA VICTIMA",
+    title="Condición de la Víctima")
 fig_mapa = px.scatter_mapbox(df_accidentes,
                              lat="LAT",
                              lon="LONG",
@@ -69,12 +114,20 @@ fig_hipotesis_ajustada = px.bar(
     x='HIPOTESIS_AJUSTADA',
     y='CUENTA',
     title='Top {} Accidentes por Hipótesis'.format(NmaxHipotesis),
-    labels={'HIPOTESIS_AJUSTADA': 'Hipótesis', 'CUENTA': 'Cantidad de Accidentes'},
+    labels={
+        'HIPOTESIS_AJUSTADA': 'Hipótesis',
+        'CUENTA': 'Cantidad de Accidentes'},
     color='CUENTA',
-    color_continuous_scale=px.colors.sequential.Blues
-)
-fig_comuna = px.pie(comunas_percent, names="COMUNA", values="PORCENTAJE", title="Porcentaje de Accidentes por Comuna (2022-2023)",
-                    labels={'COMUNA': 'Comuna', 'PORCENTAJE': '% de Accidentes'}, hole=0.3)
+    color_continuous_scale=px.colors.sequential.Blues)
+fig_comuna = px.pie(
+    comunas_percent,
+    names="COMUNA",
+    values="PORCENTAJE",
+    title="Porcentaje de Accidentes por Comuna (2022-2023)",
+    labels={
+        'COMUNA': 'Comuna',
+        'PORCENTAJE': '% de Accidentes'},
+    hole=0.3)
 
 # Crear la aplicación Dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -98,6 +151,8 @@ year_dropdown = dbc.Row(
 )
 
 # Callback para actualizar gráficos
+
+
 @app.callback(
     [Output('dia-plot', 'figure'),
      Output('mes-plot', 'figure'),
@@ -109,24 +164,52 @@ year_dropdown = dbc.Row(
      Output('comuna-plot', 'figure')],
     [Input('year-dropdown', 'value')]
 )
-
 def update_graphs(selected_year):
     filtered_df = df_accidentes[df_accidentes['AÑO'] == selected_year]
 
-    fig_dia_updated = px.histogram(filtered_df, x="DIA_SEMANA", title=f"Accidentes por Día de la Semana ({selected_year})")
-    fig_mes_updated = px.histogram(filtered_df, x="MES", title=f"Accidentes por Mes ({selected_year})")
-    fig_barrio_updated = px.histogram(filtered_df, x="BARRIOS-CORREGIMIENTO- VIA", title=f"Accidentes por Barrio ({selected_year})")
-    fig_zona_updated = px.histogram(filtered_df, x="ZONA", title=f"Accidentes por Zona ({selected_year})")
-    fig_gravedad_updated = px.pie(filtered_df, names="GRAVEDAD", title=f"Gravedad de las lesiones ({selected_year})")
-    fig_rango_edad_updated = px.histogram(filtered_df, x="RANGO_EDAD", color="GENERO",
-                                          title=f"Distribución de Accidentes por Rango de Edad y Género ({selected_year})")
-    fig_condicion_victima_updated = px.histogram(filtered_df, x="CONDICION DE LA VICTIMA",
-                                                 title=f"Condición de la Víctima ({selected_year})")
-    comunas_percent_updated = (filtered_df['COMUNA'].value_counts() / len(filtered_df) * 100).reset_index()
+    fig_dia_updated = px.histogram(
+        filtered_df,
+        x="DIA_SEMANA",
+        title=f"Accidentes por Día de la Semana ({selected_year})")
+    fig_mes_updated = px.histogram(
+        filtered_df,
+        x="MES",
+        title=f"Accidentes por Mes ({selected_year})")
+    fig_barrio_updated = px.histogram(
+        filtered_df,
+        x="BARRIOS-CORREGIMIENTO- VIA",
+        title=f"Accidentes por Barrio ({selected_year})")
+    fig_zona_updated = px.histogram(
+        filtered_df,
+        x="ZONA",
+        title=f"Accidentes por Zona ({selected_year})")
+    fig_gravedad_updated = px.pie(
+        filtered_df,
+        names="GRAVEDAD",
+        title=f"Gravedad de las lesiones ({selected_year})")
+    fig_rango_edad_updated = px.histogram(
+        filtered_df,
+        x="RANGO_EDAD",
+        color="GENERO",
+        title=f"Distribución de Accidentes por Rango de Edad y Género ({selected_year})")
+    fig_condicion_victima_updated = px.histogram(
+        filtered_df,
+        x="CONDICION DE LA VICTIMA",
+        title=f"Condición de la Víctima ({selected_year})")
+    comunas_percent_updated = (
+        filtered_df['COMUNA'].value_counts() /
+        len(filtered_df) *
+        100).reset_index()
     comunas_percent_updated.columns = ['COMUNA', 'PORCENTAJE']
-    fig_comuna_updated = px.pie(comunas_percent_updated, names="COMUNA", values="PORCENTAJE",
-                                title=f"Porcentaje de Accidentes por Comuna ({selected_year})",
-                                labels={'COMUNA': 'Comuna', 'PORCENTAJE': '% de Accidentes'}, hole=0.3)
+    fig_comuna_updated = px.pie(
+        comunas_percent_updated,
+        names="COMUNA",
+        values="PORCENTAJE",
+        title=f"Porcentaje de Accidentes por Comuna ({selected_year})",
+        labels={
+            'COMUNA': 'Comuna',
+            'PORCENTAJE': '% de Accidentes'},
+        hole=0.3)
 
     return fig_dia_updated, fig_mes_updated, fig_barrio_updated, fig_zona_updated, fig_gravedad_updated, fig_rango_edad_updated, fig_condicion_victima_updated, fig_comuna_updated
 
@@ -137,12 +220,13 @@ def update_graphs(selected_year):
     [Input('date-picker-range', 'start_date'),
      Input('date-picker-range', 'end_date')]
 )
-
 def update_heatmap(start_date, end_date):
     # Filtrar el dataframe según las fechas seleccionadas
-    df_filtered = df_accidentes[(df_accidentes['FECHA'] >= start_date) & (df_accidentes['FECHA'] <= end_date)]
+    df_filtered = df_accidentes[(df_accidentes['FECHA'] >= start_date) & (
+        df_accidentes['FECHA'] <= end_date)]
 
-    # Agrupa por barrio y calcula la latitud y longitud promedio y cuenta los accidentes
+    # Agrupa por barrio y calcula la latitud y longitud promedio y cuenta los
+    # accidentes
     df_grouped = df_filtered.groupby('BARRIOS-CORREGIMIENTO- VIA').agg(
         LAT=pd.NamedAgg(column='LAT', aggfunc='mean'),
         LONG=pd.NamedAgg(column='LONG', aggfunc='mean'),
@@ -160,6 +244,7 @@ def update_heatmap(start_date, end_date):
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return fig
+
 
 # Componente de selección de fecha para el heatmap
 date_picker = dbc.Row(
